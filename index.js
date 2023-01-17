2624ee8ea909719658eface6ad7ac8c66e9dbc58
iAlexia <122884332+ialexia@users.noreply.github.com>
2023-01-17 14:01:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const express = require("express")
 const app = express()
 
-app.get("/", (req, res) => {
-	res.send("Hello world!")
-})
-
 app.get("/proxy", (req, res) => {
 	res.send("You're not supposed to be here!")
 })
